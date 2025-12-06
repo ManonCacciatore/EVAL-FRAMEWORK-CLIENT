6495958b2008d44200e627df117b35658a6c194a
“ManonCacciatore” <cacciatoremanon8@gmail.com>
2025-12-06 17:26:57 +0100

    Components ui MyButton

---
+++ src/components/ui/MyButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    type: { type: String, default: "button" },
+});
+</script>
+
+<template>
+    <button :type="type" class="mt-4 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <slot />
+    </button>
+</template>
+
+<style scoped></style>
